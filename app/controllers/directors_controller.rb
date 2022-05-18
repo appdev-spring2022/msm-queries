080bfc9e10dd6ba0a6623eb42574be6cf74a8daa
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-05-18 23:47:21 +0000

    V1

---
+++ app/controllers/directors_controller.rb
@@ -0,0 +1,22 @@
+class DirectorsController < ApplicationController
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({:id => the_id}).at(0)
+    render({:template => "directors_template/show.html.erb"})
+  end
+  
+  def index
+    @list_of_directors = Director.all
+    render({:template => "directors_template/index.html.erb"})
+  end
+
+  def wisest
+    @oldest = Director.where.not({:dob => nil}).order({:dob => :asc}).at(0)
+    render({:template => "directors_template/eldest.html.erb"})
+  end
+
+  def newest
+    @youngest = Director.where.not({:dob => nil}).order({:dob => :desc}).at(0)
+    render({:template => "directors_template/youngest.html.erb"})
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
+  get("/directors", {:controller => "directors", :action => "index"})
+  get("/directors/eldest", {:controller => "directors", :action => "wisest"})
+  get("/directors/youngest", {:controller => "directors", :action => "newest"})
 end
