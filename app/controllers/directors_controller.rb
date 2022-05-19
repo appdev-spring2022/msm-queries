0d9c64014bba4100092218380ae91456bec89ea7
utkarshgarg92 <ugarg@chicagobooth.edu>
2022-05-19 20:14:24 +0000

    V3

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -1,11 +1,4 @@
 class DirectorsController < ApplicationController
-  def director_details
-    the_id = params.fetch("an_id")
-    @the_director = Director.where({:id => the_id}).at(0)
-    @filmography = Movie.where({:director_id => the_id})
-    render({:template => "directors_template/show.html.erb"})
-  end
-  
   def index
     @list_of_directors = Director.all
     render({:template => "directors_template/index.html.erb"})
@@ -19,5 +12,12 @@ class DirectorsController < ApplicationController
   def newest
     @youngest = Director.where.not({:dob => nil}).order({:dob => :desc}).at(0)
     render({:template => "directors_template/youngest.html.erb"})
+  end
+  
+  def director_details
+    the_id = params.fetch("an_id")
+    @the_director = Director.where({:id => the_id}).at(0)
+    @filmography = Movie.where({:director_id => the_id})
+    render({:template => "directors_template/show.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
   get("/directors", {:controller => "directors", :action => "index"})
   get("/directors/eldest", {:controller => "directors", :action => "wisest"})
   get("/directors/youngest", {:controller => "directors", :action => "newest"})
   get("/", {:controller => "application", :action => "homepage"})
+  get("/directors/:an_id", { :controller => "directors", :action => "director_details" })
 end
